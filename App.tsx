aa0bfbf56dd7c1f2bb1d19c336eb8efda6e770cf
Nikita <todti@jinteki.dev>
2022-11-12 07:23:21 +0500

    added Montserrat font

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
 // import MainTabsScreen from './screens/MainTabsScreen';
+import { useFonts } from 'expo-font';
 import WizardStack from './screens/WizardScreen/WizardScreen';
+import theme from './theme';
 
-const App = () => (
-  <NativeBaseProvider>
-    <NavigationContainer>
-      {/* <MainTabsScreen /> */}
-      <WizardStack />
-    </NavigationContainer>
-  </NativeBaseProvider>
-);
+const App = () => {
+  const [loaded] = useFonts({
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        {/* <MainTabsScreen /> */}
+        <WizardStack />
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+};
 
 export default App;
