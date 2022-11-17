4d8797e0c373e36ea99fcf5d3bb80cd7c6655a8b
Nikita <todti@jinteki.dev>
2022-11-17 20:36:03 +0500

    fixed date formatting in date picker

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -15,7 +15,7 @@ const formatDate = (date?: Date): string => {
   if (!date) return '';
 
   const day = date.getDate().toString().padStart(2, '0');
-  const month = date.getMonth().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
 
   return `${day}.${month}.${year}`;
